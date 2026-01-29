601cca062dfdeb13474b720fcd381a5583954fba
João Vitor Pereira da Costa <joaovitor.pcosta2005@gmail.com>
2026-01-29 14:18:21 -0300

    Time: 10 ms (96.45%), Space: 80.8 MB (5.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -5,17 +5,9 @@
 var containsDuplicate = function(nums) {
     let m = new Map();
 
-    for (let i = 0; i < nums.length; i++){
-        console.log("i = ",i)
-        if (m.has(nums[i])){
-            console.log("tem = ", nums[i])
-            // m.set[`${nums[i]}`, m.get('key') += 1];
-            return true
-        }
-        else{
-            console.log("Não tem= ", nums[i])
-            m.set(nums[i], 1); 
-        }
+    for (let num of nums){
+        if (m.has(num)) { return true;} 
+        m.set(num, 1); 
     }
 
     return false
